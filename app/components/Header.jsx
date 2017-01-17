ac54710ad378e1697e3f4a5650e2eef48a608eeb
Dillon Powers <dillon.c.powers@gmail.com>
2017-01-17 17:54:31 -0500

    adjs cycling but no css transitions

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -6,6 +6,34 @@ import { toggleLanguage } from '../ducks/language';
 class Header extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      English: ['developer', 'runner', 'linguist'],
+      Mandarin: ['黑客', '跑步爱好者', '语言学家'],
+      pointer: 0,
+      interval: null
+    };
+
+    this.changePointer = this.changePointer.bind(this);
+  }
+
+  componentWillMount() {
+    this.setState(prevState => {
+      if (prevState.interval) {
+        clearInterval(prevState.interval);
+        return { interval: null };
+      }
+      let interval = setInterval(this.changePointer, 2000);
+      return { interval };
+    })
+  }
+
+  changePointer() {
+    this.setState(prevState => {
+      let adjLen = prevState.English.length - 1;
+      let plusOne = prevState.pointer + 1;
+      let pointer = (plusOne > adjLen) ? 0 : plusOne;
+      return { pointer };
+    })
   }
 
   render() {
@@ -17,8 +45,11 @@ class Header extends Component {
           <button id="btn-language" onClick={handleToggle}>{ (language === 'English') ? '汉语' : 'English' }</button>
         </div>
         <div id="top-container">
-          <div id="name-container" className="center-horiz">
-            <h1>{ (language === 'English') ? 'Dillon Powers' : '彭郎' }</h1>
+          <div id="video-overlay" className="center-horiz">
+            <div id="title-container">
+              <h1>{ (language === 'English') ? 'Dillon Powers' : '彭郎' }</h1>
+              <p>{this.state[language][this.state.pointer]}</p>
+            </div>
           </div>
           <div id="video-container">
             <video playsInline autoPlay muted loop id="wheel-vid">
@@ -27,7 +58,7 @@ class Header extends Component {
           </div>
         </div>
         <div className="big">
-          <p>paragraph that takes up a bunch of space</p>
+          <h3>paragraph that takes up a bunch of space</h3>
         </div>
       </div>
     );
