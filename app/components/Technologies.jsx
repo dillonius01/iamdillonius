69304263ec5bc6e0736690a8360515e1b64a8f81
Dillon Powers <dillon.c.powers@gmail.com>
2017-02-09 11:41:23 -0500

    centered and responsive

--- app/components/Technologies.jsx
+++ app/components/Technologies.jsx
@@ -21,8 +21,8 @@ const Technologies = ({ language }) => (
 
 			<h2 className="txt-center txt-intro">
 				{ (language === 'English') ?
-					"I built this site from scratch with React-Redux and Node" :
-					"本网站是我自己亲手做的，主要的框架是React-Redux与Node"
+					"I built this site from scratch with React, Redux, and Node" :
+					"本网站是我自己亲手做的，主要的框架是React,Redux与Node"
 				}
 			</h2>
 
@@ -35,36 +35,36 @@ const Technologies = ({ language }) => (
 		</div>
 		<div className="col-12">
 			
-			<div className="col-3 inline-block">
-				<img className="fit" src="https://nodejs.org/static/images/logos/nodejs-new-pantone-black.png" alt=""/>
+			<div className="col-6 md-col md-col-3 inline-block tech-img-container">
+				<img className="tech-img" src="https://nodejs.org/static/images/logos/nodejs-new-pantone-black.png" alt=""/>
 			</div>
 
-			<div className="col-3 inline-block">
-				<img className="fit" src="http://daynin.github.io/clojurescript-presentation/img/react-logo.png" alt=""/>
+			<div className="col-6 md-col md-col-3 inline-block tech-img-container">
+				<img className="tech-img" src="http://daynin.github.io/clojurescript-presentation/img/react-logo.png" alt=""/>
 			</div>
 
-			<div className="col-3 inline-block">
-				<img className="fit" src="https://raw.githubusercontent.com/reactjs/redux/master/logo/logo.png" alt=""/>
+			<div className="col-6 md-col md-col-3 inline-block tech-img-container">
+				<img className="tech-img" src="https://raw.githubusercontent.com/reactjs/redux/master/logo/logo.png" alt=""/>
 			</div>
 
-			<div className="col-3 inline-block">
-				<img className="fit" src="https://lh3.googleusercontent.com/-whXBCDVxIto/Vz2Rsyz-UjI/AAAAAAAAiJc/UjvR-M2b9tY5SyKFkDY6Q_MbusEINRXkQ/w1024-h1024/Firebase_16-logo.png" alt=""/>
+			<div className="col-6 md-col md-col-3 inline-block tech-img-container">
+				<img className="tech-img" src="https://lh3.googleusercontent.com/-whXBCDVxIto/Vz2Rsyz-UjI/AAAAAAAAiJc/UjvR-M2b9tY5SyKFkDY6Q_MbusEINRXkQ/w1024-h1024/Firebase_16-logo.png" alt=""/>
 			</div>
 
-			<div className="col-3 inline-block">
-				<img className="fit" src="https://git-scm.com/images/logos/downloads/Git-Logo-2Color.png" alt=""/>
+			<div className="col-6 md-col md-col-3 inline-block tech-img-container">
+				<img className="tech-img" src="https://git-scm.com/images/logos/downloads/Git-Logo-2Color.png" alt=""/>
 			</div>
 
-			<div className="col-3 inline-block">
-				<img className="fit" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Postgresql_elephant.svg/1000px-Postgresql_elephant.svg.png" alt=""/>
+			<div className="col-6 md-col md-col-3 inline-block tech-img-container">
+				<img className="tech-img" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Postgresql_elephant.svg/1000px-Postgresql_elephant.svg.png" alt=""/>
 			</div>
 
-			<div className="col-3 inline-block">
-				<img className="fit" src="http://mean.io/wp-content/themes/twentysixteen-child/images/express.png" alt=""/>
+			<div className="col-6 md-col md-col-3 inline-block tech-img-container">
+				<img className="tech-img" src="http://mean.io/wp-content/themes/twentysixteen-child/images/express.png" alt=""/>
 			</div>
 			
-			<div className="col-3 inline-block">
-				<img className="fit" src="https://onsen.io/blog/content/images/2015/Aug/chaijs-mocha.png" alt=""/>
+			<div className="col-6 md-col md-col-3 inline-block tech-img-container">
+				<img className="tech-img" src="https://onsen.io/blog/content/images/2015/Aug/chaijs-mocha.png" alt=""/>
 			</div>
 			
 		</div>
